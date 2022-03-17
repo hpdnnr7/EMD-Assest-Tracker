2f340ddd095d201a3ebd9d94865d8ac55298a372
Hope Dunner <hpdnnr7@gmail.com>
2022-03-17 15:30:33 -0400

    updated alerts

--- js/record-of-transfer.js
+++ js/record-of-transfer.js
@@ -15,12 +15,10 @@ $('document').ready(() => {
 					<td><input id='description${count}' type='text'/></td>
                     <td><input id='cost${count}' type='text'/></td>
                     <td><input id='po_no${count}' type='text'/></td>
-                    <td><input id='remarks${count}' type='text'/></td>
+                    <td><remark><input id='remarks${count}' type='text'/></remark></td>
 					<td><button type="button" index="${count}" class="btn btn-danger btn-remove">X</button></td>
 
 
-
-					
 				</tr>
 		`;
 
@@ -41,34 +39,45 @@ $('document').ready(() => {
 	// Handler to Submit Data
 	$('#submit').click(() =>{
 		//Checks if all heading information is filled out
-		const date = document.getElementById('date_out').value;
-		const department = document.getElementById('department_out').value;
-		const division = document.getElementById('division_out').value;
-		const resp_ctr = document.getElementById('resp_out').value;
-		const prop_ctr = document.getElementById('prop_out').value;
-		const dep_head = document.getElementById('dhead_out').value;
+		const date_out = document.getElementById('date_out').value;
+		const department_out = document.getElementById('department_out').value;
+		const division_out = document.getElementById('division_out').value;
+		const resp_out = document.getElementById('resp_out').value;
+		const prop_out = document.getElementById('prop_out').value;
+		const dhead_out = document.getElementById('dhead_out').value;
 		
-		if (date == '') {
-			alert('The Date section can not be left blank');
-		}
+		const date_in = document.getElementById('date_in').value;
+		const department_in = document.getElementById('department_in').value;
+		const division_in = document.getElementById('division_in').value;
+		const resp_in = document.getElementById('resp_in').value;
+		const prop_in = document.getElementById('prop_in').value;
+		const dhead_in = document.getElementById('dhead_in').value;
 		
-		if (department == '') {
-			alert('Department section can not be left blank');
-		}
-		if (division == '') {
-			alert('Division section can not be left blank');
-		}
-		
-		if (resp_ctr == '') {
-			alert('Resp. Ctr section can not be left blank');
+		let errorMessage = '';
+		$('#errorDiv').html(errorMessage);
+
+		if ((date_out == '') || (date_in == '')) {
+			errorMessage += '<p>The Date section can not be left blank</p>';
+			//alert('The Date section can not be left blank');
 		}
 
-		if (prop_ctr == '') {
-			alert('Property Control Office section can not be left blank');
+		if ((department_out == '') || (department_in == '')) {
+			//alert('The Department section can not be left blank');
+		}
+		if ((division_out == '') || (division_in == '')) {
+			//alert('The Division section can not be left blank');
+		}
+		
+		if ((resp_out == '') || (resp_in == '')) {
+			//alert('Resp. Ctr section can not be left blank');
 		}
 
-		if (dep_head == '') {
-			alert('Department/Division Head section can not be left blank');
+		if ((prop_out == '') || (prop_in == '')) {
+			//alert('Property Control Office section can not be left blank');
+		}
+
+		if ((dhead_out == '') || (dhead_in == '')) {
+			//alert('Department/Division Head section can not be left blank');
 		}
 
 		//Format all Data into JSON for Submission
@@ -82,22 +91,13 @@ $('document').ready(() => {
 
 			// Store all Info from this row
 			let assetInfo = {
-				x: $(`department${i}`).val(),
+
 				asset_tag_no: $(`#asset_tag_no${i}`).val(),
 				manufacturer: $(`#manufacturer_serial_no${i}`).val(),
 				descriptions: $(`#description${i}`).val(),
 				costs: $(`#cost${i}`).val(),
 				po_no: $(`#po_no${i}`).val(),
 				remarks: $(`#remarks${i}`).val(),
-
-
-				// date:$(`#date${i}`).val(),
-				// department: $(`#department${i}`).val(),
-				// division: $(`#division${i}`).val(),
-				// resp_ctr: $(`#resp_ctr${i}`).val(),
-				// property_control: $(`#property_control${i}`).val(),
-				// department_division: $(`#department_division${i}`).val(),
-
 				
 
 			}
@@ -105,37 +105,47 @@ $('document').ready(() => {
 
 			// TODO: Check for Empty Inputs and Enforce Users to Fill out Fields
 					
-					if (assetInfo.asset_tag_no == '') {
-						console.log(assetInfo.asset_tag_no);
-						alert('Asset Tag Number can not be left blank');
-					}
-					if (assetInfo.manufacturer == ''){  
-						alert('Manufacturer Serial Number can not be left blank');
-					}
-					if (assetInfo.descriptions == ''){  
-						alert('The Description can not be left blank');
-					}
-					if (assetInfo.costs == ''){  
-						alert('The Cost can not be left blank');
-					}  
-					if (assetInfo.po_no == ''){  
-						alert('The P.O Number/ Document Number can not be left blank');
-					}   
-					if (assetInfo.remarks == ''){  
-						alert('The Remarks can not be left blank');
-					}
-					if (assetInfo.asset_tag_no && assetInfo.manufacturer && assetInfo.descriptions && assetInfo.costs && assetInfo.po_no && assetInfo.department_division_head != ''){  
-						alert('Submission Accepted');
-					
-			// Add Info to array
-				data.push(assetInfo);
-					}
-				}	
+			if (assetInfo.asset_tag_no == '') {
+				//alert('Asset Tag Number can not be left blank');
+			}
+			if (assetInfo.manufacturer == ''){  
+				//alert('Manufacturer Serial Number can not be left blank');
+			}
+			if (assetInfo.descriptions == ''){  
+				//alert('The Description can not be left blank');
+			}
+			if (assetInfo.costs == ''){  
+				//alert('The Cost can not be left blank');
+			}  
+			if (assetInfo.po_no == ''){  
+				//alert('The P.O Number/ Document Number can not be left blank');
+			}   
+			if (assetInfo.remarks == ''){  
+				//alert('The Remarks can not be left blank');
+			}
 
-		//TODO: Upload JSON to Database
-		console.log(data);
 
-	})
+		}
+		
+		
+		if (errorMessage != ''){
+			$('#errorDiv').html(errorMessage);
+			// Code to display error message on page
+		}
+		else{
+			// Submit and show success message
+			
+		}
+		if (assetInfo.asset_tag_no && assetInfo.manufacturer && assetInfo.descriptions && assetInfo.costs && assetInfo.po_no && assetInfo.remarks  != ''){  
+			alert('Submission Accepted');
+		
+// Add Info to array
+	data.push(assetInfo);
+		}
+//TODO: Upload JSON to Database
+console.log(data);
+
+})
 
 
 
